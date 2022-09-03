a5fda1ca892c6f26a6f6a0983ffdb5fcd5a5d681
joackson santos <joackson3@gmail.com>
2022-09-03 12:24:52 -0300

    first commit

---
+++ Form2.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace ConversordeSegundos
+{
+    partial class fmrConverterSegundos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblSegundosResultado = new System.Windows.Forms.Label();
+            this.lblMinutosResultado = new System.Windows.Forms.Label();
+            this.lblHorasRsrultado = new System.Windows.Forms.Label();
+            this.lblSegundos = new System.Windows.Forms.Label();
+            this.lblMinutos = new System.Windows.Forms.Label();
+            this.lblHora = new System.Windows.Forms.Label();
+            this.btnConversor = new System.Windows.Forms.Button();
+            this.txtBoxSegundos = new System.Windows.Forms.TextBox();
+            this.lblTexto = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblSegundosResultado
+            // 
+            this.lblSegundosResultado.BackColor = System.Drawing.Color.Gainsboro;
+            this.lblSegundosResultado.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblSegundosResultado.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblSegundosResultado.ForeColor = System.Drawing.Color.Red;
+            this.lblSegundosResultado.Location = new System.Drawing.Point(344, 275);
+            this.lblSegundosResultado.Name = "lblSegundosResultado";
+            this.lblSegundosResultado.Size = new System.Drawing.Size(61, 21);
+            this.lblSegundosResultado.TabIndex = 16;
+            this.lblSegundosResultado.Visible = false;
+            // 
+            // lblMinutosResultado
+            // 
+            this.lblMinutosResultado.BackColor = System.Drawing.Color.Gainsboro;
+            this.lblMinutosResultado.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblMinutosResultado.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblMinutosResultado.ForeColor = System.Drawing.Color.Red;
+            this.lblMinutosResultado.Location = new System.Drawing.Point(194, 276);
+            this.lblMinutosResultado.Name = "lblMinutosResultado";
+            this.lblMinutosResultado.Size = new System.Drawing.Size(61, 21);
+            this.lblMinutosResultado.TabIndex = 15;
+            this.lblMinutosResultado.Visible = false;
+            // 
+            // lblHorasRsrultado
+            // 
+            this.lblHorasRsrultado.BackColor = System.Drawing.Color.Gainsboro;
+            this.lblHorasRsrultado.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblHorasRsrultado.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblHorasRsrultado.ForeColor = System.Drawing.Color.Red;
+            this.lblHorasRsrultado.Location = new System.Drawing.Point(65, 276);
+            this.lblHorasRsrultado.Name = "lblHorasRsrultado";
+            this.lblHorasRsrultado.Size = new System.Drawing.Size(61, 21);
+            this.lblHorasRsrultado.TabIndex = 14;
+            this.lblHorasRsrultado.Visible = false;
+            // 
+            // lblSegundos
+            // 
+            this.lblSegundos.AutoSize = true;
+            this.lblSegundos.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblSegundos.Location = new System.Drawing.Point(411, 276);
+            this.lblSegundos.Name = "lblSegundos";
+            this.lblSegundos.Size = new System.Drawing.Size(89, 21);
+            this.lblSegundos.TabIndex = 13;
+            this.lblSegundos.Text = "Segundo(s)";
+            this.lblSegundos.Visible = false;
+            // 
+            // lblMinutos
+            // 
+            this.lblMinutos.AutoSize = true;
+            this.lblMinutos.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblMinutos.Location = new System.Drawing.Point(261, 276);
+            this.lblMinutos.Name = "lblMinutos";
+            this.lblMinutos.Size = new System.Drawing.Size(77, 21);
+            this.lblMinutos.TabIndex = 12;
+            this.lblMinutos.Text = "Minuto(s)";
+            this.lblMinutos.Visible = false;
+            // 
+            // lblHora
+            // 
+            this.lblHora.AutoSize = true;
+            this.lblHora.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblHora.Location = new System.Drawing.Point(132, 276);
+            this.lblHora.Name = "lblHora";
+            this.lblHora.Size = new System.Drawing.Size(61, 21);
+            this.lblHora.TabIndex = 11;
+            this.lblHora.Text = "Hora(s)";
+            this.lblHora.Visible = false;
+            // 
+            // btnConversor
+            // 
+            this.btnConversor.Location = new System.Drawing.Point(628, 154);
+            this.btnConversor.Name = "btnConversor";
+            this.btnConversor.Size = new System.Drawing.Size(107, 23);
+            this.btnConversor.TabIndex = 10;
+            this.btnConversor.Text = "Converter";
+            this.btnConversor.UseVisualStyleBackColor = true;
+            this.btnConversor.Click += new System.EventHandler(this.btnConversor_Click);
+            // 
+            // txtBoxSegundos
+            // 
+            this.txtBoxSegundos.Location = new System.Drawing.Point(461, 154);
+            this.txtBoxSegundos.Name = "txtBoxSegundos";
+            this.txtBoxSegundos.Size = new System.Drawing.Size(142, 23);
+            this.txtBoxSegundos.TabIndex = 9;
+            // 
+            // lblTexto
+            // 
+            this.lblTexto.Font = new System.Drawing.Font("Segoe UI", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblTexto.Location = new System.Drawing.Point(55, 151);
+            this.lblTexto.Name = "lblTexto";
+            this.lblTexto.Size = new System.Drawing.Size(400, 41);
+            this.lblTexto.TabIndex = 17;
+            this.lblTexto.Text = "Digite a quantidade de segundos a ser convertida:";
+            // 
+            // fmrConverterSegundos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblTexto);
+            this.Controls.Add(this.lblSegundosResultado);
+            this.Controls.Add(this.lblMinutosResultado);
+            this.Controls.Add(this.lblHorasRsrultado);
+            this.Controls.Add(this.lblSegundos);
+            this.Controls.Add(this.lblMinutos);
+            this.Controls.Add(this.lblHora);
+            this.Controls.Add(this.btnConversor);
+            this.Controls.Add(this.txtBoxSegundos);
+            this.Name = "fmrConverterSegundos";
+            this.Text = "Conversor De Segundos";
+            this.Load += new System.EventHandler(this.fmrConverterSegundos_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label lblSegundosResultado;
+        private Label lblMinutosResultado;
+        private Label lblHorasRsrultado;
+        private Label lblSegundos;
+        private Label lblMinutos;
+        private Label lblHora;
+        private Button btnConversor;
+        private TextBox txtBoxSegundos;
+        private Label lblTexto;
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConversordeSegundos
+{
+    public partial class fmrConverterSegundos : Form
+    {
+        public fmrConverterSegundos()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnConversor_Click(object sender, EventArgs e)
+        {
+            int valor, horas, minutos, segundos;
+            valor = Convert.ToInt32(txtBoxSegundos.Text);
+
+            if (valor >= 60){
+                segundos = valor % 60;
+                valor = valor / 60;
+            }
+            else{
+                segundos = valor;
+                valor = 0;
+            }
+
+            if (valor < 60){
+                minutos = valor;
+                valor = 0;
+            }
+            else{
+                minutos = valor % 60;
+                valor = valor / 60;
+            }
+
+            if (valor< 60){
+                horas = valor;
+                valor = 0;
+            }
+            else{
+                horas = valor % 60;
+               
+            }
+            
+            lblHora.Visible = true;
+            lblHorasRsrultado.Visible = true;
+            lblMinutos.Visible = true;
+            lblMinutosResultado.Visible = true;
+            lblSegundos.Visible = true;
+            lblSegundosResultado.Visible = true;
+
+            lblHorasRsrultado.Text = Convert.ToString(horas);
+            lblMinutosResultado.Text = Convert.ToString(minutos);
+            lblSegundosResultado.Text = Convert.ToString(segundos);
+        }
+
+        private void fmrConverterSegundos_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
